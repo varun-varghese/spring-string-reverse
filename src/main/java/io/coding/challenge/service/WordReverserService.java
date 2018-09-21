8f3c7cf60c4a8dbc386e1fd4d83e79dd2d94e19c
varun varghese <varun.koshy.varghese@vgmail.com>
2018-09-21 13:30:06 -0500

    removed code

--- src/main/java/io/coding/challenge/service/WordReverserService.java
+++ src/main/java/io/coding/challenge/service/WordReverserService.java
@@ -3,7 +3,6 @@ package io.coding.challenge.service;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
-import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -14,38 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class WordReverserService {
-	
-	public String reverseWithSb(String input) {
-		log.debug("| Reversing: {}", input);
-		StringBuilder out = new StringBuilder();
-		StringTokenizer spaceTkn = new StringTokenizer(input, " ");
-		int tokenStartIndex = -1;
-		while (spaceTkn.hasMoreTokens()) {
-	        StringBuilder thisToken = new StringBuilder(spaceTkn.nextToken());
-	        StringTokenizer splTkn = new StringTokenizer(thisToken.toString(), ".!,?:;'\"!@#$%^&*(){}[]|");
-	        
-	        int splTokenStartIndex = -1;
-	        while(splTkn.hasMoreTokens()) {
-	        	StringBuilder thisSplToken = new StringBuilder(splTkn.nextToken());
-	        	out.append(thisSplToken.reverse());
-
-	        	splTokenStartIndex += thisSplToken.length() + 1;
-	        	if (thisToken.length() > splTokenStartIndex) {
-	        		out.append(thisToken.charAt(splTokenStartIndex));
-	        	}
-	        }
-	        
-	        tokenStartIndex += thisToken.length() + 1;
-	        if (input.length() > tokenStartIndex) {
-	        	out.append(input.charAt(tokenStartIndex));
-	        	log.trace("| Token index: {}, char:{}, in:{}, Out:{}", tokenStartIndex, input.charAt(tokenStartIndex), thisToken.toString(), out.toString());
-	        }
-	        
-	    }
-		String result = out.toString().trim();
-	    log.info("| After reversing: {}", result);
-        return result;
-	}
 	
 	public String reverse(String input) {
 		List<Character> outChar = new LinkedList<Character>();
